e7f564604381124603fabfd3bffdc8e933544ef9
A.Sikorski <sikorski.aps@gmail.com>
2021-01-04 00:48:01 +0100

    Updated security settings

--- covidmindserver/settings.py
+++ covidmindserver/settings.py
@@ -22,10 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(qake@oo53_a96+w*r+0wv*0^wu843jvvqkq=+fb#y+@$h$9_a'
+try:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+except KeyError:
+    SECRET_KEY = 'secret-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['covid-mind.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -85,8 +88,11 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.parse(os.environ['DATABASE_URL'], conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+try:
+    db_from_env = dj_database_url.parse(os.environ['DATABASE_URL'], conn_max_age=600)
+    DATABASES['default'].update(db_from_env)
+except KeyError:
+    pass
 
 
 # Password validation
