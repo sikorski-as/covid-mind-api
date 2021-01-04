eebd700d529fcfb0ca2f8466fa79ebe660a95f64
A.Sikorski <sikorski.aps@gmail.com>
2021-01-04 01:41:21 +0100

    Updated added local settings

---
+++ covidmindserver/local_settings.py
@@ -0,0 +1,12 @@
+from .settings import *
+
+SECRET_KEY = 'debug-secret-key'
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
